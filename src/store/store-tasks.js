ad57f7cefe25c815e376f27dfe253ff5f4ab4414
LucianoBuhler <lucianobuhler@gmail.com>
2024-11-18 14:52:05 -0300

    Up and running

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -170,8 +170,8 @@ const getters = {
         keysOrdered = Object.keys(state.tasks)
 
       keysOrdered.sort((a, b) => {
-        let taskAProp = state.tasks[a].[state.sort].toLowerCase(),
-            taskBProp = state.tasks[b].[state.sort].toLowerCase()
+        let taskAProp = state.tasks[a][state.sort].toLowerCase(),
+            taskBProp = state.tasks[b][state.sort].toLowerCase()
 
         if (taskAProp > taskBProp) return 1
         else if (taskAProp < taskBProp) return -1
